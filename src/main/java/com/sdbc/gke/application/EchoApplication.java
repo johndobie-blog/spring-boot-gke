78ec33cf1f16965e7371944f5f31459813a0fc02
John Dobie <john@dobie.co.uk>
2020-03-11 18:28:51 +0000

    add host to show the loadbalancer working

--- src/main/java/com/sdbc/gke/application/EchoApplication.java
+++ src/main/java/com/sdbc/gke/application/EchoApplication.java
@@ -1,23 +1,48 @@
 package com.sdbc.gke.application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+
 @SpringBootApplication
 @RestController
+@Configuration
+@EnableConfigurationProperties
 public class EchoApplication {
+
+    private String ipAddress;
 
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
 
-    @GetMapping("/echo")
-    public static String echo(@RequestParam String message){
-        return message;
+
+    public EchoApplication() {
+        super();
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not get local address");
+        }
     }
 
+    @GetMapping("/echo")
+    public String echo(@RequestParam String message, HttpServletRequest request) {
+        return ipAddress + ":" + message;
+    }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
